118a17b3d15b3bbb404ce23891aadca6270ee8d5
antonio-wachave <anthonypaulino32@gmail.com>
2025-05-24 18:25:49 +0200

    Databases seed update

---
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -0,0 +1,49 @@
+package com.devsuperior.dslist.entities;
+
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
+@Entity
+@Table(name="tb_belonging")
+public class Belonging {
+
+    @EmbeddedId
+    private BelongingPk belongingPk = new BelongingPk();
+    private Integer position;
+
+    public Belonging() {}
+
+    public Belonging(Game game, GameList gameList, Integer position) {
+        belongingPk.setGame(game);
+        belongingPk.setGameList(gameList);
+        this.position = position;
+    }
+
+    public BelongingPk getBelongingPk() {
+        return belongingPk;
+    }
+
+    public void setBelongingPk(BelongingPk belongingPk) {
+        this.belongingPk = belongingPk;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return "Belonging{" +
+                "belongingPk=" + belongingPk +
+                ", position=" + position +
+                '}';
+    }
+}
