e77a078ce8ba478a12a41ee80d5b02c969d3a19e
antonio-wachave <anthonypaulino32@gmail.com>
2025-05-24 16:25:14 +0200

    controller update

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,10 +1,27 @@
 package com.devsuperior.dslist.controllers;
 
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class GameController {
 
+    private final GameService gameService;
+
+    @Autowired
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @GetMapping("/games")
+    public List<GameMinDTO> findAll(){
+        return gameService.findAll();
+    }
 }
